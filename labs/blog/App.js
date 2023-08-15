e49554fcebb6d5f1eb42b3575df0e27109fda53d
vitavl6686 <vita@trousev.pro>
2023-08-15 10:22:02 +0100

    initial setup of context in blog applciation

--- labs/blog/App.js
+++ labs/blog/App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import React from "react";
+import IndexScreen from "./src/screens/IndexScreen";
+import { BlogProvider } from "./src/context/BlogContext";
+const navigator = createStackNavigator({
+  Index: IndexScreen,
+  }, {
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+    title: 'Blogs'
+  }
 });
+
+const App = createAppContainer(navigator)
+export default () => {
+  return(
+  <BlogProvider>
+      <App />
+  </BlogProvider>
+  )
+};
